cbcc57578c59bf5de45e9a4fe1c4eabe2c27b261
Dr Monkey <deniel1000@gmail.com>
2022-07-05 18:58:48 +0800

    git push

---
+++ footer.js
@@ -0,0 +1,6 @@
+footer = '<!-- Footer開始 -->\
+<footer class="py-5" style="background: #eee;">\
+  <div class="text-center ">© 2022 Phoenix 寵物溝通公司 版權所有.</div>\
+</footer>\
+<!-- Footer結束 -->\
+document.write(footer)';
---
+++ script.js
@@ -0,0 +1,37 @@
+script= '<nav class="navbar navbar-expand-lg navbar-light bg-light">\
+  <div class="container">\
+    <a class="navbar-brand d-flex align-items-center" href="#">\
+      <img src="img/logo.png" alt="" class="logo">\
+      &emsp;Phoenix 寵物溝通\
+    </a>\
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"\
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
+      <span class="navbar-toggler-icon"></span>\
+    </button>\
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">\
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">\
+        <li class="nav-item">\
+          <a class="nav-link" aria-current="page" href="Index.html">首頁</a>\
+        </li>\
+        <li class="nav-item">\
+          <a class="nav-link" href="experience.html">個人經歷</a>\
+        </li>\
+        <li class="nav-item">\
+          <a class="nav-link" href="service.html">服務項目</a>\
+        </li>\
+        <li class="nav-item">\
+          <a class="nav-link" href="feedback.html">飼主回饋</a>\
+        </li>\
+        <li class="nav-item">\
+          <a class="nav-link" href="contact_us.html">聯絡我們</a>\
+        </li>\
+      </ul>\
+    </div>\
+  </div>\
+</nav>\
+<!--navbar 結束-->\
+<!-- body 開始 -->\
+<header class="position-relative"></header>\
+<!-- body 結束 -->\
+document.write(script.js)';
+
